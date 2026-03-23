c5cd75b229493c80aad9c8099967108871233a40
Rustambek Begmetov <begmetovrustambek@gmail.com>
2026-03-23 15:09:45 +0500

    Responding to questions is working

--- handlers/feedback.py
+++ handlers/feedback.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F, Bot
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import BaseFilter, CommandStart
+from aiogram.types import Message, ReactionTypeEmoji
 
 from database.sql import db
 
@@ -14,6 +14,10 @@ def format_message(text: str, message: Message):
         first_name=message.from_user.first_name,
         last_name=message.from_user.last_name,
     ))
+
+class AdminFilter(BaseFilter):
+    async def __call__(self, message: Message, bot: Bot):
+        return message.from_user.id in db.admins_list(bot.token)
 
 
 def feedback_router():
@@ -32,7 +36,20 @@ def feedback_router():
         await message.answer(msg)
 
         if message.from_user.id in db.admins_list(bot.token):
-            await message.answer("For admins: You can answer to questions by replying to messages")
+            await message.answer("You can answer to questions by replying to messages <i>(This message won't be displayed to users)</i>")
+
+
+    @router.message(AdminFilter())
+    async def respond(message: Message, bot: Bot):
+        try:
+            await bot.copy_message(
+                message.reply_to_message.forward_from.id,
+                message.from_user.id,
+                message.message_id
+            )
+            await message.react([ReactionTypeEmoji(emoji="👍")])
+        except:
+            await message.answer("You aren't replied to a message")
 
 
     @router.message(F.text)
@@ -41,8 +58,10 @@ def feedback_router():
             await bot.forward_message(admin, message.from_user.id, message.message_id)
         await message.reply("Thank you")
 
+
     @router.message(~F.text)
     async def only_text(message: Message):
         await message.answer("Sorry, only text messages are accepted")
+
 
     return router
